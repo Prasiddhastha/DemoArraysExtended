303713cdc9ce13200edee2565b66bd2a57f2f6bf
Prasiddha Shrestha <prasiddhastha7@gmail.com>
2023-01-06 16:35:52 +0545

    Add project files.

---
+++ DemoArraysExtended/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace DemoArrays
+{
+    class Program
+    { 
+    static void Main(string[] args)
+        {
+            var numbers = new[] { 3, 4, 7, 12, 14, 6 };
+            //Lentgh 
+            Console.WriteLine($"length: {numbers.Length}");
+            //indexof()
+            var index = Array.IndexOf(numbers, 12);
+            Console.WriteLine($"index of 12 is {index}");
+            //Clear()
+            Array.Clear(numbers, 0, 3);
+            Console.WriteLine("Effect of Clear()");
+            foreach(var number in numbers )
+            {
+                Console.WriteLine(number);
+            }
+            //Copy()
+            var another = new int[4];
+            Array.Copy(numbers, another, 4);
+            Console.WriteLine("Effect of Copy()");
+            foreach(var number in another )
+            {
+                Console.WriteLine(number);
+            }
+            //Sort()
+            Array.Sort(numbers);
+            Console.WriteLine("Effect of Sort()");
+                foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            //Reverse
+            Array.Reverse(numbers);
+            Console.WriteLine("Effect of Reverse()");
+                foreach(var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
